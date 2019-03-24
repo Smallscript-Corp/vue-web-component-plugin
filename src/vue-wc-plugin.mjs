b8f1ded6f82f82b878b6284dc701afd73157bcfb
dsim <dsim@smallscript.com>
2019-03-23 17:43:37 -0700

    initial check-in.

---
+++ src/vue-wc-plugin.mjs
@@ -0,0 +1,102 @@
+/* 
+  Copyright Smallscript, and David Simmons (c) 2019
+  
+  MIT License: https://github.com/Smallscript-Corp/vue-web-component-plugin
+*/
+export default 
+class WebComponent extends HTMLElement {
+  static get isEnabled() { return this.$isEnabled; }
+  static set isEnabled(value) {
+    if(this.$isEnabled = value) {
+      Object.keys(this.deferred).forEach(
+        key => { this.deferred[key].register(); });
+    }
+    return true;
+  }
+  static shim_component(Vue) {
+    const $this = this;
+    const $component = this.$component = Vue.component;
+    Vue.component = function(tagName, vueDefinition) {
+      if(vueDefinition && 'shadowCss' in vueDefinition) {
+        class CustomWebComponent extends $this { };
+        CustomWebComponent.vueTagName = tagName;
+        CustomWebComponent.vueDefinition = vueDefinition;
+        if(!$this.isEnabled) {
+          this.config.ignoredElements.push(tagName);
+          $this.deferred[tagName] = CustomWebComponent;
+        }
+        else
+          CustomWebComponent.register();
+        return vueDefinition;
+      }
+      return($component.apply(this, arguments));
+    }
+  }
+  static install(Vue, args) {
+    // `install` is effectively our static constructor
+    this.$plugin = this; this.deferred = {}; this.$args = args;
+    this.isEnabled = (args && 'isEnabled' in args) ? args.isEnabled : true;
+    this.Vue = Vue; this.$isDOMSlotAttr = Vue.$isDOMSlotAttr;
+    this.shim_component(Vue);
+  }
+  static markSlotElementsForTreatmentAsNativeDOM(node) {
+    if(node.tagName === 'SLOT' && !node.hasAttribute(this.$isDOMSlotAttr))
+      node.setAttribute(this.$isDOMSlotAttr,'');
+    for(var cel = node.firstElementChild; cel; cel = cel.nextElementSibling)
+      this.markSlotElementsForTreatmentAsNativeDOM(cel);
+  }
+  static register() {
+    // Fixup the vueDefinition as needed; saving original, then manipulating it
+    const vueDefinition = this.vueDefinition;
+    const rootEl = document.createElement('div');
+    rootEl.innerHTML = this.vueDefinition.$template = vueDefinition.template;
+    this.markSlotElementsForTreatmentAsNativeDOM(rootEl);
+    vueDefinition.template = (rootEl.childElementCount > 1)
+      ? rootEl.outerHTML
+      : rootEl.innerHTML;
+    window.customElements.define(this.vueTagName, this);
+  }
+  
+  constructor () {
+    super();
+    this.$wcs = { // record shadowRoot; which allows "closed" to work
+      shadowRoot: this.attachShadow({mode:'open'}), 
+    };
+    const vueDefinition = this.vueDefinition;
+    if(typeof vueDefinition.shadowCss === 'string' && vueDefinition.shadowCss.length) {
+      const shadowStyle = document.createElement('style');
+      shadowStyle.type = 'text/css';
+      shadowStyle.appendChild(document.createTextNode(vueDefinition.shadowCss));
+      this.$wcs.shadowRoot.appendChild(shadowStyle);
+    }
+    this.$wcs.shadowRoot.appendChild(document.createElement('div'));
+  }
+  get vueDefinition() { return this.constructor.vueDefinition; }
+  getParentVueOf(el) {
+    if(!el)
+      return undefined;
+    else if(el.__vue__) 
+      return el.__vue__;
+    else if(el.constructor === ShadowRoot)
+      return this.getParentVueOf(el.host);
+    else
+      return this.getParentVueOf(el.parentNode);
+  }
+  connectedCallback() {
+    if(!this.isConnected || this.$wcs.vue) return;
+    let el = this.$wcs.shadowRoot.firstElementChild;
+    if(el.nextElementSibling) el = el.nextElementSibling;
+    const parent = this.getParentVueOf(this);
+    this.$wcs.vue = new Vue(this.$wcs.vueDefinition = {
+      __proto__: this.vueDefinition,
+      el: el,
+      parent: parent,
+    });
+  }
+  disconnectedCallback() {
+    // Remove from this.$wcs.vue.parent
+  }
+  adoptedCallback() {
+    // Reattach to new parent: this.$wcs.vue.parent
+  }
+};
